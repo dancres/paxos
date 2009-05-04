e67de9a958d44817adf0b9e4f4104acf70f371dd
Dan Creswell <dan.creswell@gmail.com>
2009-05-04 08:29:33 +0100

    Curse default naming policies in Netbeans.

--- src/org/dancres/paxos/impl/core/LogStorage.java
+++ src/org/dancres/paxos/impl/core/LogStorage.java
@@ -1,6 +1,6 @@
 package org.dancres.paxos.impl.core;
 
 public interface LogStorage {
-    public byte[] get(long _seqNum);
-    public void put(long _seqNum, byte[] value);
+    public byte[] get(long aSeqNum);
+    public void put(long aSeqNum, byte[] aValue);
 }
