d28075926cf849d341a1974c5ca8e29189d8db90
Dan Creswell <dan.creswell@gmail.com>
2011-09-02 18:58:04 +0100

    Initial Paxos interface and factory implementation (still need to use a real logger).

--- src/main/java/org/dancres/paxos/PaxosFactory.java
+++ src/main/java/org/dancres/paxos/PaxosFactory.java
@@ -1,7 +1,18 @@
 package org.dancres.paxos;
 
-import org.dancres.paxos.impl.CheckpointHandle;
+import java.net.InetSocketAddress;
 
+import org.dancres.paxos.impl.CheckpointHandle;
+import org.dancres.paxos.impl.Core;
+import org.dancres.paxos.impl.netty.TransportImpl;
+import org.dancres.paxos.impl.util.MemoryLogStorage;
+
+/**
+ * @todo Convert to use HowlLogger
+ * 
+ * @author dan
+ *
+ */
 public class PaxosFactory {
     /**
      * An application should first load it's last available snapshot which should include an instance of a
@@ -11,11 +22,46 @@ public class PaxosFactory {
      * @param aListener that will receive agreed messages, snapshots etc.
      * @param aHandle is the handle last given to the application as result of a snapshot.
      */
-    public static Paxos init(Paxos.Listener aListener, CheckpointHandle aHandle) {
+    public static Paxos init(Paxos.Listener aListener, CheckpointHandle aHandle) throws Exception {
         throw new UnsupportedOperationException();
     }
 
-    public static Paxos init(Paxos.Listener aListener, CheckpointHandle aHandle, byte[] aMetaData) {
-        throw new UnsupportedOperationException();
+    public static Paxos init(Paxos.Listener aListener, CheckpointHandle aHandle, byte[] aMetaData) throws Exception {
+    	return new PaxosImpl(aListener, aHandle, aMetaData);
+    }
+    
+    private static class PaxosImpl implements Paxos {
+    	private Core _core;
+    	private TransportImpl _transport;
+    	
+    	PaxosImpl(Paxos.Listener aListener, CheckpointHandle aHandle, byte[] aMetaData) throws Exception {
+    		_core = new Core(5000, new MemoryLogStorage(), aMetaData, aListener);
+    		_transport = new TransportImpl();
+    		_transport.add(_core);
+    	}
+    	
+		public void close() {
+			_core.stop();
+		}
+
+		public CheckpointHandle newCheckpoint() {
+			return _core.getAcceptorLearner().newCheckpoint();
+		}
+
+		public void submit(Proposal aValue) {
+			_core.submit(aValue);
+		}
+
+		public void register(Listener aListener) {
+			_core.getAcceptorLearner().add(aListener);
+		}
+
+		public void bringUpToDate(CheckpointHandle aHandle) throws Exception {
+			_core.getAcceptorLearner().bringUpToDate(aHandle);
+		}
+
+		public byte[] getMetaData(InetSocketAddress anAddress) {
+			return _core.getFailureDetector().getMetaData(anAddress);
+		}
     }
 }
