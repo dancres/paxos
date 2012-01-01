e043d7ff6a3f4a030ca20d5b9a5c6ae019d4a755
Dan Creswell <dan.creswell@gmail.com>
2012-01-01 18:01:55 +0000

    Test classes for Jackson and Spark

---
+++ src/test/java/org/dancres/paxos/test/Jackson.java
@@ -0,0 +1,30 @@
+package org.dancres.paxos.test;
+
+
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Jackson {
+    public static void main(String[] anArgs) throws Exception {
+        ObjectMapper myMapper = new ObjectMapper();
+
+        Map myTable = new HashMap<String, String>();
+        myTable.put("node1", "aURL");
+        myTable.put("node2", "anotherURL");
+
+        ByteArrayOutputStream myBAOS = new ByteArrayOutputStream();
+
+        myMapper.writeValue(myBAOS, myTable);
+
+        ByteArrayInputStream myBAIS = new ByteArrayInputStream(myBAOS.toByteArray());
+        
+        Map<String, Object> myRead = myMapper.readValue(myBAIS, Map.class);
+        
+        for (Map.Entry e: myRead.entrySet())
+            System.out.println(e.getKey() + " -> " + e.getValue());
+    }
+}
---
+++ src/test/java/org/dancres/paxos/test/Sparkup.java
@@ -0,0 +1,18 @@
+package org.dancres.paxos.test;
+
+import static spark.Spark.*;
+import spark.*;
+
+public class Sparkup {
+
+    public static void main(String[] args) {
+
+        get(new Route("/hello") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "Hello World!";
+            }
+        });
+
+    }
+}
