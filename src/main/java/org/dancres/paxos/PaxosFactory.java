9911c4800f5f85b54ac734481e1fb3ed66f9dd1f
Dan Creswell <dan.creswell@gmail.com>
2012-01-02 19:54:37 +0000

    Explicit method for controlling LogStorage type on PaxosFactory

--- src/main/java/org/dancres/paxos/PaxosFactory.java
+++ src/main/java/org/dancres/paxos/PaxosFactory.java
@@ -2,6 +2,7 @@ package org.dancres.paxos;
 
 import org.dancres.paxos.impl.CheckpointHandle;
 import org.dancres.paxos.impl.Core;
+import org.dancres.paxos.impl.LogStorage;
 import org.dancres.paxos.impl.netty.TransportImpl;
 import org.dancres.paxos.impl.util.MemoryLogStorage;
 
@@ -22,6 +23,14 @@ public class PaxosFactory {
      */
     public static Paxos init(Paxos.Listener aListener, CheckpointHandle aHandle, byte[] aMetaData) throws Exception {
         Core myCore = new Core(5000, new MemoryLogStorage(), aMetaData, aHandle, aListener);
+        new TransportImpl().add(myCore);
+
+        return myCore;
+    }
+
+    public static Paxos init(Paxos.Listener aListener, CheckpointHandle aHandle, byte[] aMetaData,
+                             LogStorage aLogger) throws Exception {
+        Core myCore = new Core(5000, aLogger, aMetaData, aHandle, aListener);
         new TransportImpl().add(myCore);
 
         return myCore;
