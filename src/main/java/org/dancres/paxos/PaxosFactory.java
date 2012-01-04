c718a272b0936522d035aa2d33ac157c9274e594
Dan Creswell <dan.creswell@gmail.com>
2012-01-04 12:54:52 +0000

    Remove redundant comments.

--- src/main/java/org/dancres/paxos/PaxosFactory.java
+++ src/main/java/org/dancres/paxos/PaxosFactory.java
@@ -7,20 +7,9 @@ import org.dancres.paxos.impl.netty.TransportImpl;
 import org.dancres.paxos.impl.util.MemoryLogStorage;
 
 /**
- * @todo Convert to use HowlLogger
- * 
  * @author dan
- *
  */
 public class PaxosFactory {
-    /**
-     * An application should first load it's last available snapshot which should include an instance of a
-     * SnapshotHandle which should be passed to <code>init</code>. If the application has no previous snapshot
-     * and no thus no valid <code>SnapshotHandle</code> it should pass SnapshotHandle.NO_SNAPSHOT
-     *
-     * @param aListener that will receive agreed messages, snapshots etc.
-     * @param aHandle is the handle last given to the application as result of a snapshot.
-     */
     public static Paxos init(Paxos.Listener aListener, CheckpointHandle aHandle, byte[] aMetaData) throws Exception {
         Core myCore = new Core(5000, new MemoryLogStorage(), aMetaData, aHandle, aListener);
         new TransportImpl().add(myCore);
