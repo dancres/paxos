88cf7b8a4f65806c1430a588a66d97e2fec59595
Dan Creswell <dan.creswell@gmail.com>
2012-01-06 17:15:55 +0000

    Fix test inline with CheckpointStorage rename.

--- src/test/java/org/dancres/paxos/test/junit/CheckpointStorageTest.java
+++ src/test/java/org/dancres/paxos/test/junit/CheckpointStorageTest.java
@@ -1,6 +1,6 @@
 package org.dancres.paxos.test.junit;
 
-import org.dancres.paxos.test.rest.CheckpointStorage;
+import org.dancres.paxos.test.rest.DirectoryCheckpointStorage;
 import org.dancres.paxos.test.utils.FileSystem;
 import org.junit.Assert;
 import org.junit.Before;
@@ -18,18 +18,18 @@ public class CheckpointStorageTest {
     }
     
     @Test public void test() throws Exception {
-        CheckpointStorage myStorage = new CheckpointStorage(new File(_checkpointDir));
+        DirectoryCheckpointStorage myStorage = new DirectoryCheckpointStorage(new File(_checkpointDir));
 
         File[] myFiles = myStorage.getFiles();
         Assert.assertTrue(myFiles.length == 0);
 
-        CheckpointStorage.WriteCheckpoint myCkpt = myStorage.newCheckpoint();
+        DirectoryCheckpointStorage.WriteCheckpoint myCkpt = myStorage.newCheckpoint();
         ObjectOutputStream myOOS = new ObjectOutputStream(myCkpt.getStream());
         myOOS.writeObject(new Integer(55));
         myOOS.close();
         myCkpt.saved();
 
-        CheckpointStorage.ReadCheckpoint myRestore = myStorage.getLastCheckpoint();
+        DirectoryCheckpointStorage.ReadCheckpoint myRestore = myStorage.getLastCheckpoint();
         ObjectInputStream myOIS = new ObjectInputStream(myRestore.getStream());
         Assert.assertTrue(new Integer(55).equals(myOIS.readObject()));
         myOIS.close();
