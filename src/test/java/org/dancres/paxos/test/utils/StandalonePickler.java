4aaedc456d1e08118cf9bf4d21a4c9311f22393e
Dan Creswell <dan.creswell@gmail.com>
2012-11-04 17:36:55 +0000

    Sort out indentation.

--- src/test/java/org/dancres/paxos/test/utils/StandalonePickler.java
+++ src/test/java/org/dancres/paxos/test/utils/StandalonePickler.java
@@ -9,46 +9,46 @@ import org.dancres.paxos.messages.PaxosMessage;
 
 public class StandalonePickler implements Transport.PacketPickler {
     public byte[] pickle(Transport.Packet aPacket) {
-		byte[] myBytes = Codecs.encode(aPacket.getMessage());
-		ByteBuffer myBuffer = ByteBuffer.allocate(8 + 4 + myBytes.length);
-		
-		myBuffer.putLong(Codecs.flatten(aPacket.getSource()));
-		myBuffer.putInt(myBytes.length);
-		myBuffer.put(myBytes);
-		myBuffer.flip();
-		
-		return myBuffer.array();
+        byte[] myBytes = Codecs.encode(aPacket.getMessage());
+        ByteBuffer myBuffer = ByteBuffer.allocate(8 + 4 + myBytes.length);
+        
+        myBuffer.putLong(Codecs.flatten(aPacket.getSource()));
+        myBuffer.putInt(myBytes.length);
+        myBuffer.put(myBytes);
+        myBuffer.flip();
+        
+        return myBuffer.array();
     }
 
     public Transport.Packet unpickle(byte[] aBytes) {
-		ByteBuffer myBuffer = ByteBuffer.wrap(aBytes);
-		
-		InetSocketAddress mySource = Codecs.expand(myBuffer.getLong());
-		int myLength = myBuffer.getInt();
-		byte[] myPaxosBytes = new byte[myLength];
-		myBuffer.get(myPaxosBytes);
-		
-		PaxosMessage myMessage = Codecs.decode(myPaxosBytes);
-		
-		return new PacketImpl(myMessage, mySource);        	
+        ByteBuffer myBuffer = ByteBuffer.wrap(aBytes);
+        
+        InetSocketAddress mySource = Codecs.expand(myBuffer.getLong());
+        int myLength = myBuffer.getInt();
+        byte[] myPaxosBytes = new byte[myLength];
+        myBuffer.get(myPaxosBytes);
+        
+        PaxosMessage myMessage = Codecs.decode(myPaxosBytes);
+        
+        return new PacketImpl(myMessage, mySource);         
     }
 
     class PacketImpl implements Transport.Packet {
-    	private PaxosMessage _msg;
-    	private InetSocketAddress _source;
-    	
-    	PacketImpl(PaxosMessage aMsg, InetSocketAddress aSource) {
-    		_msg = aMsg;
-    		_source = aSource;
-    	}
-    	
-		public InetSocketAddress getSource() {
-    		return _source;
-		}
+        private PaxosMessage _msg;
+        private InetSocketAddress _source;
+        
+        PacketImpl(PaxosMessage aMsg, InetSocketAddress aSource) {
+            _msg = aMsg;
+            _source = aSource;
+        }
+        
+        public InetSocketAddress getSource() {
+            return _source;
+        }
 
-		public PaxosMessage getMessage() {
-			return _msg;
-		}		
+        public PaxosMessage getMessage() {
+            return _msg;
+        }       
     }
 }
 
