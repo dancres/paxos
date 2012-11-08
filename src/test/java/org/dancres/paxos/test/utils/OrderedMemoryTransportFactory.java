6c72547b697dd8776154d570058931f8d7e11848
Dan Creswell <dan.creswell@gmail.com>
2012-11-08 11:03:26 +0000

    First pass impl of a deterministic in-memory network for testing purposes.

---
+++ src/test/java/org/dancres/paxos/test/utils/OrderedMemoryTransportFactory.java
@@ -0,0 +1,99 @@
+package org.dancres.paxos.test.utils;
+
+import org.dancres.paxos.impl.Transport;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetSocketAddress;
+
+import java.util.Map;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class OrderedMemoryTransportFactory implements Runnable {
+    private static Logger _logger = LoggerFactory.getLogger(OrderedMemoryTransportFactory.class);
+
+    private class PacketWrapper {
+        private Transport.Packet _packet;
+        private InetSocketAddress _target;
+
+        PacketWrapper(Transport.Packet aPacket, InetSocketAddress aTarget) {
+            _packet = aPacket;
+            _target = aTarget;
+        }
+
+        Transport.Packet getPacket() {
+            return _packet;
+        }
+
+        InetSocketAddress getTarget() {
+            return _target;
+        }
+    }
+
+    private BlockingQueue<PacketWrapper> _queue = new LinkedBlockingQueue<PacketWrapper>();
+    private AtomicBoolean _isStopping = new AtomicBoolean(false);
+    private InetSocketAddress  _broadcastAddr;
+    private Map<InetSocketAddress, OrderedMemoryTransportImpl> _nodes = new ConcurrentHashMap<InetSocketAddress, OrderedMemoryTransportImpl>();
+
+    public OrderedMemoryTransportFactory() throws Exception {
+        _broadcastAddr = new InetSocketAddress(org.dancres.paxos.impl.net.Utils.getBroadcastAddress(), 255);
+
+        Thread myDispatcher = new Thread(this);
+
+        myDispatcher.setDaemon(true);
+        myDispatcher.start();
+    }
+
+    public void stop() {
+        _isStopping.set(true);
+    }
+
+    void enqueue(Transport.Packet aPacket, InetSocketAddress aTarget) throws Exception {
+        _queue.put(new PacketWrapper(aPacket, aTarget));
+    }
+
+    public void run() {
+        while (! _isStopping.get()) {
+            try {
+                PacketWrapper myNext = _queue.poll(100, TimeUnit.MILLISECONDS);
+
+                if (myNext != null) {
+                    if (myNext.getTarget().equals(_broadcastAddr)) {
+                        for (InetSocketAddress k : _nodes.keySet()) {
+                            dispatch(new PacketWrapper(myNext.getPacket(), k));
+                        }
+                    } else {
+                        dispatch(myNext);
+                    }
+                }
+            } catch (Exception anE) {
+                _logger.error("Failed to dispatch queue", anE);
+            }
+        }
+    }
+
+    private void dispatch(PacketWrapper aPayload) {
+        OrderedMemoryTransportImpl myDest = _nodes.get(aPayload.getTarget());
+
+        if (myDest != null)
+            myDest.distribute(aPayload.getPacket());
+        else
+            _logger.warn("Couldn't distribute packet to target: " + aPayload.getTarget());
+    }
+
+    public Transport newTransport() {
+        InetSocketAddress myAddr = Utils.getTestAddress();
+        OrderedMemoryTransportImpl myTrans = new OrderedMemoryTransportImpl(myAddr, _broadcastAddr, this);
+
+        _nodes.put(myAddr, myTrans);
+
+        return myTrans;
+    }
+}
---
+++ src/test/java/org/dancres/paxos/test/utils/OrderedMemoryTransportImpl.java
@@ -0,0 +1,126 @@
+package org.dancres.paxos.test.utils;
+
+import org.dancres.paxos.impl.Transport;
+import org.dancres.paxos.impl.Stream;
+import org.dancres.paxos.messages.PaxosMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetSocketAddress;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class OrderedMemoryTransportImpl implements Transport {
+	private static Logger _logger = LoggerFactory.getLogger(OrderedMemoryTransportImpl.class);
+
+	private OrderedMemoryTransportFactory _factory;
+	private PacketPickler _pickler = new StandalonePickler();
+	private Set<Dispatcher> _dispatcher = new HashSet<Dispatcher>();
+    private AtomicBoolean _isStopping = new AtomicBoolean(false);
+	private InetSocketAddress _unicastAddr;
+    private InetSocketAddress _broadcastAddr;
+
+    public OrderedMemoryTransportImpl(InetSocketAddress aLocalAddr, InetSocketAddress aBroadAddr, OrderedMemoryTransportFactory aFactory) {
+    	_unicastAddr = aLocalAddr;
+    	_broadcastAddr = aBroadAddr;
+    	_factory = aFactory;
+    }
+
+    public PacketPickler getPickler() {
+    	return _pickler;
+    }
+
+	private void guard() {
+		if (_isStopping.get())
+			throw new IllegalStateException("Transport is stopped");
+	}
+	
+    public void add(Dispatcher aDispatcher) throws Exception {
+    	guard();
+    	
+        synchronized(this) {
+            _dispatcher.add(aDispatcher);
+            aDispatcher.setTransport(this);
+        }
+    }
+
+	public InetSocketAddress getLocalAddress() {
+		return _unicastAddr;
+	}
+
+    public InetSocketAddress getBroadcastAddress() {
+    	return _broadcastAddr;
+    }
+	
+    /**
+     * @param aMessage is the message to send
+     * @param anAddr is the address of the target for the message which might be <code>Address.BROADCAST</code>.
+     */
+    public void send(PaxosMessage aMessage, InetSocketAddress anAddr) {
+		guard();
+		
+		try {
+			_factory.enqueue(new FakePacket(_unicastAddr, aMessage), anAddr);			
+		} catch (Exception anE) {
+			_logger.error("Failed to write message", anE);
+		}
+    }
+
+    void distribute(Transport.Packet aPacket) {
+    	synchronized(this) {
+            for(Dispatcher d : _dispatcher) {
+                if (d.messageReceived(aPacket))
+                    break;
+            }
+    	}    	
+    }
+
+	private class StreamImpl implements Stream {
+		private InetSocketAddress _target;
+		private AtomicBoolean _closed = new AtomicBoolean(false);
+
+		StreamImpl(InetSocketAddress aTarget) {
+			_target = aTarget;
+		}
+		
+		public void close() {
+			_closed.set(true);
+		}
+		
+		public void send(PaxosMessage aMessage) {
+			if (_closed.get())
+				throw new RuntimeException("Stream is closed");
+
+			try {
+				_factory.enqueue(new FakePacket(_unicastAddr, aMessage), _target);
+			} catch (Exception anE) {
+				_logger.error("Couldn't enqueue packet", anE);
+			}
+		}
+
+		public void sendRaw(Packet aPacket) {
+			if (_closed.get())
+				throw new RuntimeException("Stream is closed");
+
+			try {
+				_factory.enqueue(aPacket, _target);
+			} catch (Exception anE) {
+				_logger.error("Couldn't enqueue packet", anE);
+			}
+		}
+	}
+	
+	public void connectTo(final InetSocketAddress aNodeId, final ConnectionHandler aHandler) {
+		guard();
+		
+        aHandler.connected(new StreamImpl(aNodeId));
+	}
+
+    public void shutdown() {
+		_isStopping.set(true);
+    }	
+}
