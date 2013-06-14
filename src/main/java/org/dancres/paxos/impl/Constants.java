016381636bf992ef14b0e2141de70e30ce47d5b1
Dan Creswell <dan.creswell@gmail.com>
2013-06-14 17:05:19 +0100

    Trim a dead constant.

--- src/main/java/org/dancres/paxos/impl/Constants.java
+++ src/main/java/org/dancres/paxos/impl/Constants.java
@@ -2,8 +2,7 @@ package org.dancres.paxos.impl;
 
 public abstract class Constants {
 	private static final long DEFAULT_LEADER_LEASE = 30000;
-	
-    public static final long NO_ROUND = -1;
+
     public static final long UNKNOWN_SEQ = -1;
     private static volatile long _leaderLease = DEFAULT_LEADER_LEASE;
 
