59f0802994714e7ad515bee80740b85422d112ac
Dan Creswell <dan.creswell@gmail.com>
2013-12-22 15:22:23 +0000

    Add a test for AccepLedger

---
+++ src/test/java/org/dancres/paxos/impl/AcceptLedgerTest.java
@@ -0,0 +1,21 @@
+package org.dancres.paxos.impl;
+
+import junit.framework.Assert;
+import org.dancres.paxos.Proposal;
+import org.dancres.paxos.messages.Accept;
+import org.dancres.paxos.messages.Begin;
+import org.dancres.paxos.test.net.FakePacket;
+import org.junit.Test;
+
+public class AcceptLedgerTest {
+    @Test
+    public void breakLedger() throws Exception {
+        AcceptLedger myAL = new AcceptLedger(1);
+
+        myAL.add(new FakePacket(new Accept(1, 2)));
+        myAL.add(new FakePacket(new Accept(1, 2)));
+        myAL.add(new FakePacket(new Accept(1, 2)));
+
+        Assert.assertNull(myAL.tally(new Begin(1, 2, new Proposal("key", new byte[0])), 3));
+    }
+}
