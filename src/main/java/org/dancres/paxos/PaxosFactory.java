8f874e5661651b3c583372eb717dd334c2738d2c
Dan Creswell <dan.creswell@gmail.com>
2014-06-09 13:39:13 +0100

    Dump dead method in PaxosFactory

--- src/main/java/org/dancres/paxos/PaxosFactory.java
+++ src/main/java/org/dancres/paxos/PaxosFactory.java
@@ -12,24 +12,6 @@ public class PaxosFactory {
      * @param aListener is the listener to pass <code>StateEvent</code>'s to
      * @param aHandle is the handle for the most recent checkpoint taken by the user-code
      * @param aClientId is the id for the user-code service instance request a <code>Paxos</code> instance.
-     *
-     * @throws Exception
-     */
-    public static Paxos init(int aClusterSize,
-                             Listener aListener, CheckpointHandle aHandle, byte[] aClientId) throws Exception {
-        Core myCore = new Core(new MemoryLogStorage(), aHandle, aListener);
-        Transport myTransport = new TransportImpl(new FailureDetectorImpl(aClusterSize, 5000), aClientId);
-        myTransport.routeTo(myCore);
-        myCore.init(myTransport);
-
-        return myCore;
-    }
-
-    /**
-     * @param aClusterSize is the size of the cluster
-     * @param aListener is the listener to pass <code>StateEvent</code>'s to
-     * @param aHandle is the handle for the most recent checkpoint taken by the user-code
-     * @param aClientId is the id for the user-code service instance request a <code>Paxos</code> instance.
      * @param aLogger is the logger implementation to use for the <code>Paxos</code> log. Typically this would be
      *                persistent.
      *
