c04a2731d8f8445a1c3dfd94a31dde41eb41df87
Dan Creswell <dan.creswell@gmail.com>
2014-12-23 14:44:31 +0000

    Avoid lock around user-code callback otherwise deadlock risk

--- src/main/java/org/dancres/paxos/storage/MemoryLogStorage.java
+++ src/main/java/org/dancres/paxos/storage/MemoryLogStorage.java
@@ -3,16 +3,16 @@ package org.dancres.paxos.storage;
 import org.dancres.paxos.LogStorage;
 
 import java.util.Iterator;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class MemoryLogStorage implements LogStorage {
-    private long _nextKey = 0;
-    private final SortedMap<Long, byte[]> _log = new TreeMap<>();
+    private AtomicLong _nextKey = new AtomicLong(0);
+    private final ConcurrentSkipListMap<Long, byte[]> _log = new ConcurrentSkipListMap<>();
 
     private boolean isClosed = false;
     private boolean isOpened = false;
-    
+
 	public void close() throws Exception {
 		synchronized(this) {
 			assert (isOpened);
@@ -23,21 +23,19 @@ public class MemoryLogStorage implements LogStorage {
 	}
 
 	public void mark(long key, boolean force) throws Exception {
-		synchronized(this) {
-			Iterator<Long> myKeys = _log.keySet().iterator();
-			while (myKeys.hasNext()) {
-				Long myKey = myKeys.next();
-				if (myKey < key)
-					myKeys.remove();
-			}
-		}
+        Iterator<Long> myKeys = _log.keySet().iterator();
+        while (myKeys.hasNext()) {
+            Long myKey = myKeys.next();
+            if (myKey < key)
+                myKeys.remove();
+        }
 	}
 
 	public void open() throws Exception {
 		synchronized(this) {
 			assert (!isOpened);
 			assert (!isClosed);
-			
+
 			isOpened = true;
 		}
 	}
@@ -46,35 +44,36 @@ public class MemoryLogStorage implements LogStorage {
 		synchronized(this) {
 			assert (isOpened);
 			assert (!isClosed);
-			
-			long myKey = _nextKey;
-			
-			++_nextKey;			
-			_log.put(myKey, data);
-			
-			return myKey;
 		}
+
+        long myKey = _nextKey.getAndIncrement();
+
+        _log.put(myKey, data);
+
+        return myKey;
 	}
 
 	public byte[] get(long position) throws Exception {
 		synchronized(this) {
 			assert (isOpened);
 			assert (!isClosed);
-			
-			return _log.get(position);
 		}
+
+        return _log.get(position);
 	}
-	
+
 	public void replay(RecordListener listener, long mark) throws Exception {
 		synchronized(this) {
 			assert (isOpened);
 			assert (!isClosed);
+		}
 
-            for (Long myKey : _log.keySet()) {
-                if (myKey >= mark) {
-                    listener.onRecord(myKey, _log.get(myKey));
-                }
-            }
+        ConcurrentSkipListMap<Long, byte[]> myCopy = _log.clone();
+
+		for (Long myKey : myCopy.keySet()) {
+			if (myKey >= mark) {
+				listener.onRecord(myKey, myCopy.get(myKey));
+			}
 		}
 	}
 }
