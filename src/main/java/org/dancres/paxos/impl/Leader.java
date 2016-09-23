1bb532b4c09d9f9d0a8b2f2031209dc3347551aa
Dan Creswell <dan.creswell@gmail.com>
2016-09-23 17:06:40 +0100

    Add commentary around the number of outcomes for a ballot

--- src/main/java/org/dancres/paxos/impl/Leader.java
+++ src/main/java/org/dancres/paxos/impl/Leader.java
@@ -142,6 +142,16 @@ class Leader implements Instance {
         }
     }
 
+    /**
+     * There is usually a single outcome, good or bad. However, there may be as yet uncompleted ballots and in such
+     * a case the leader will be expected to drive those to completion. When this happens, the value submitted in
+     * the current request may be superseded by another "hanging" from the uncompleted ballots. The fact that this
+     * has occurred is disclosed in the form of two outcomes. The first will be a report of an OTHER_VALUE, the second
+     * will be a report of VALUE. The former will be the value in the current request, the latter will be the
+     * "hanging" value from a previous ballot.
+     *
+     * @return the set of outcomes that occurred at this ballot
+     */
     Deque<VoteOutcome> getOutcomes() {
         return _outcomes;
     }
