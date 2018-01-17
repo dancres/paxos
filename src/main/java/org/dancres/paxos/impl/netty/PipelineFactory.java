b1aa9bdda41f410a965e53849567b601a0fbb6b0
Dan Creswell <dan.creswell@gmail.com>
2018-01-17 12:50:55 +0000

    Remove superfluous classes post netty 4.1.19 migration

--- src/main/java/org/dancres/paxos/impl/netty/PipelineFactory.java
+++
@@ -1,9 +0,0 @@
-package org.dancres.paxos.impl.netty;
-
-import org.dancres.paxos.impl.Transport;
-import org.jboss.netty.channel.ChannelPipeline;
-import org.jboss.netty.channel.SimpleChannelHandler;
-
-interface PipelineFactory {
-    ChannelPipeline newPipeline(Transport.PacketPickler aPickler, SimpleChannelHandler aHandler);
-}
