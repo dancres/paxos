c421b40ce2a8dc96a912a30d43bc1b9dd664f7f6
Dan Creswell <dan.creswell@gmail.com>
2018-01-24 16:42:17 +0000

    Provide an empty Paxos Listener for testing purposes

--- src/main/java/org/dancres/paxos/Listener.java
+++ src/main/java/org/dancres/paxos/Listener.java
@@ -2,4 +2,9 @@ package org.dancres.paxos;
 
 public interface Listener {
     void transition(StateEvent anEvent);
+
+    /**
+     * Use this only for testing - all other Paxos instances should have fully implemented listeners
+     */
+    Listener NULL_LISTENER = (StateEvent anEvent) -> {};
 }
