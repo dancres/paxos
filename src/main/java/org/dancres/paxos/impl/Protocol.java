ea2348a4d24a093955e384533f2c46c500cefe2d
Dan Creswell <dan.creswell@gmail.com>
2018-02-12 15:20:47 +0000

    New core protocol state machine implementation and tests

---
+++ src/main/java/org/dancres/paxos/impl/Protocol.java
@@ -0,0 +1,216 @@
+package org.dancres.paxos.impl;
+
+import org.dancres.paxos.impl.net.Utils;
+import org.dancres.paxos.messages.Begin;
+import org.dancres.paxos.messages.Claim;
+import org.dancres.paxos.messages.Collect;
+
+import java.net.InetSocketAddress;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+/**
+
+ RECOVERY
+ ========
+
+ Suspend collect validation (Deny, Allow), just apply Then: aka if rnd > current_rnd store & record on disk
+
+
+ */
+public class Protocol {
+    /**
+     * Tests against the low watermark
+     */
+    interface CheckSeq<T> extends BiFunction<T, Watermark, Boolean> {
+    }
+
+    static CheckSeq<Claim> isOldSeq = (c, w) -> c.getSeqNum() < w.getSeqNum() + 1;
+    static CheckSeq<Begin> shouldWriteBegin = (b, w) -> b.getSeqNum() > w.getSeqNum();
+
+    /**
+     * Tests against the last elected Collect
+     *
+     * @param <T>
+     */
+    interface CheckRnd<T> extends BiFunction<T, Collect, Boolean> {
+    }
+
+    static CheckRnd<Claim> isPastRnd = (c, e) -> c.getRndNumber() < e.getRndNumber();
+    static CheckRnd<Collect> isElectableRnd = (c, e) -> c.getRndNumber() > e.getRndNumber();
+    static CheckRnd<Begin> isTheElectedRnd = (b, e) -> b.getRndNumber() == e.getRndNumber();
+    static CheckRnd<Collect> shouldWriteCollect = isElectableRnd;
+
+    /**
+     * Test for expiry
+     */
+    static Function<Long, Boolean> isExpired = l -> System.currentTimeMillis() > l;
+
+    /**
+     * Test for same InetSocketAddress
+     */
+    static BiFunction<InetSocketAddress, InetSocketAddress, Boolean> isSameSource = InetSocketAddress::equals;
+
+    static class StateMachine {
+        private Collect _elected = Collect.INITIAL;
+        private InetSocketAddress _elector = new InetSocketAddress(Utils.getWorkableInterfaceAddress(), 12345);
+        private long _expiry = 0;
+
+        /**
+         * COLLECT
+         * =======
+         * <p>
+         * Old:
+         * <p>
+         * rnd < current rnd => emit OldRound
+         * <p>
+         * seq < low_water + 1
+         * <p>
+         * Actionable:
+         * <p>
+         * If source is same and round >= current rnd (equal because it's possible leader will retry due to dropped packets)
+         * - DO WE NEED LEGITIMATELY TO WORRY ABOUT = ? IF WE DO IT WOULD BE BECAUSE LEADER IS GOING TO RETRY THE COLLECT DUE TO PACKET LOSS BUT SHOULD IT? AND IF IT DOES, SHOULD IT NOT JUST INCREMENT ROUND?
+         * - IT SEEMS THE ANSWER IS NO, WE SHOULD REMOVE THIS - A LEADER NOT SEEING ALL LASTS WILL TIMEOUT AND RETRY AT WHICH POINT IT WOULD RECEIVE OLD_ROUNDS AND EXIT CAUSING RESYNC OF ROUND_NUM. WHAT
+         * WE'RE CURRENTLY DOING IS SAVING SOME CYCLES BUT IT SHOULDN'T BE NECESSARY - TEST THIS TOMORROW BY REMOVING THE = FROM LEADERUTILS.SAMELEADER ROUND CHECK IN A FRESH CLONE OF THE MASTER
+         * - IN FACT WE'VE NEVER SEEN MULTI-PAXOS MESSAGE, THERE SEEMS TO BE A BUG IN AMACCEPTING() TEST. FOR A DIFFERENT LEADER WITH A NEW ROUND IT SHOULD IGNORE UNLESS OLD LEADER'S LEASE HAS EXPIRED.
+         * IN EFFECT IT SHOULD ONLY BLOCK NEW ROUNDS FROM LEADERS FOR THE DURATION OF THE LEASE. OTHERWISE THE USUAL LOGIC APPLIES WHICH MEANS THE LEADER WILL BE REJECTED BECAUSE IT'S USING AN ALREADY AGREED TO
+         * RND.
+         * - WE WILL LIKELY NEED TO RE-WIRE SOME TESTS TO FEED THE AL THE CORRECT NEXT ROUND NUMBER ABOVE LONG.MIN_VALUE AS IS CURRENTLY SET IN COLLECT.INITIAL SO WE'D NEED COLLECT.INITIAL.RNDNUMBER + 1 WHICH IS
+         * EFFECTIVELY WHAT PROPOSAL ALLOCATOR DOES FOR LEGITIMATE/NON-TEST LEADER ACTIONS ON FIRST BOOT.
+         * <p>
+         * If source is not same source and round > current rnd and leader lease expired
+         * <p>
+         * Then:
+         * <p>
+         * If rnd > current_rnd store & record on disk
+         * <p>
+         * Emit LAST for seqNum
+         * <p>
+         * NOTE: ABOVE RULES MEAN WE DON'T OLD ROUND FOR A NEW RND FROM A NEW LEADER INSIDE THE LEADER LEASE, WE'RE SILENT AS INTENDED.
+         */
+        void dispatch(Collect aProposed, InetSocketAddress aProposer, Watermark aWatermark,
+                      Outdated anOld, Act anAction) {
+
+            if (old(aProposed, _elected, aWatermark)) {
+                anOld.accept(_elected.getRndNumber(), _elector);
+            } else if (actionable(aProposed, aProposer, _elected, _elector, _expiry)) {
+                Collect myPreviouslyElected = _elected;
+                _elected = aProposed;
+                _elector = aProposer;
+
+                extendExpiry();
+                anAction.accept(aProposed, shouldWriteCollect.apply(aProposed, myPreviouslyElected));
+            }
+        }
+
+        /**
+         * BEGIN
+         * =====
+         * <p>
+         * Old:
+         * <p>
+         * rnd < current rnd => OldRound
+         * <p>
+         * seq < low_water + 1
+         * <p>
+         * Actionable:
+         * <p>
+         * If rnd match current collect
+         * <p>
+         * Then:
+         * <p>
+         * If seqNum > low_watermark store & record on disk
+         * <p>
+         * Emit accept for seqNum
+         */
+        void dispatch(Begin aBegin, Watermark aLowWatermark, Outdated anOld, Act anAction) {
+            if (old(aBegin, _elected, aLowWatermark)) {
+                anOld.accept(_elected.getRndNumber(), _elector);
+            } else if (actionable(aBegin, _elected)) {
+                extendExpiry();
+                anAction.accept(aBegin, shouldWriteBegin.apply(aBegin, aLowWatermark));
+            }
+        }
+
+        void extendExpiry() {
+            _expiry = System.currentTimeMillis() + Leader.LeaseDuration.get();
+        }
+
+        long getExpiry() {
+            return _expiry;
+        }
+        
+        /**
+         * Reject a Collect if it is past (old round), not electable (hence round not greater than current)
+         * or for an old sequence number.
+         *
+         * @param aProposedCollect
+         * @param anElectedCollect
+         * @param aLowWatermark
+         * @return
+         */
+        boolean old(Collect aProposedCollect, Collect anElectedCollect, Watermark aLowWatermark) {
+            return (isPastRnd.apply(aProposedCollect, anElectedCollect) ||
+                    (! isElectableRnd.apply(aProposedCollect, anElectedCollect)) ||
+                    (isOldSeq.apply(aProposedCollect, aLowWatermark)));
+        }
+
+        /**
+         * A Collect will be actioned if it is electable (newer round) and either the source is as previously or
+         * because the leader lease has expired (hence any source is fine).
+         *
+         * @param aProposedCollect
+         * @param aProposerSource
+         * @param anElectedCollect
+         * @param anElectedSource
+         * @param anElectedExpiry
+         * @return
+         */
+        boolean actionable(Collect aProposedCollect, InetSocketAddress aProposerSource,
+                                  Collect anElectedCollect, InetSocketAddress anElectedSource,
+                                  long anElectedExpiry) {
+            return (isElectableRnd.apply(aProposedCollect, anElectedCollect)) &&
+                    (isSameSource.apply(aProposerSource, anElectedSource) ||
+                            (isExpired.apply(anElectedExpiry)));
+        }
+
+        /**
+         * Reject a Begin if it is past (old round) or for an old sequence number
+         *
+         * @param aProposedBegin
+         * @param anElectedCollect
+         * @param aLowWatermark
+         * @return
+         */
+        boolean old(Begin aProposedBegin, Collect anElectedCollect, Watermark aLowWatermark) {
+            return ((isPastRnd.apply(aProposedBegin, anElectedCollect)) ||
+                    (isOldSeq.apply(aProposedBegin, aLowWatermark)));
+        }
+
+        /**
+         * A Begin will be actioned if it comes from the currently elected leader. If it doesn't and it's not past
+         * or old, we should not action and remain quiet because the related collect hasn't been seen and we are
+         * behind ledger actions.
+         * 
+         * @param aProposedBegin
+         * @param anElectedCollect
+         * @return
+         */
+        boolean actionable(Begin aProposedBegin, Collect anElectedCollect) {
+            return isTheElectedRnd.apply(aProposedBegin, anElectedCollect);
+        }
+    }
+
+    /**
+     * Report that a Claim has been made against an old round using the currently elected round and the elector
+     */
+    interface Outdated extends BiConsumer<Long, InetSocketAddress> {
+    }
+
+    /**
+     * Act on the provided Claim and log it if directed
+     */
+    interface Act extends BiConsumer<Claim, Boolean> {
+    }
+}
---
+++ src/test/java/org/dancres/paxos/impl/ProtocolTest.java
@@ -0,0 +1,235 @@
+package org.dancres.paxos.impl;
+
+import org.dancres.paxos.messages.Begin;
+import org.dancres.paxos.messages.Claim;
+import org.dancres.paxos.messages.Collect;
+import org.dancres.paxos.test.net.Utils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.net.InetSocketAddress;
+
+public class ProtocolTest {
+    @Test
+    public void sanity() {
+        Assert.assertEquals("Critical condition for tests breached [SEQ]",
+                Constants.PRIMORDIAL_SEQ, -1);
+        Assert.assertEquals("Critical condition for tests breached [RND]",
+                Constants.PRIMORDIAL_RND, Long.MIN_VALUE);
+    }
+
+    @Test
+    public void validateWatermarkChecks() {
+        Collect myBefore = new Collect(4, Constants.PRIMORDIAL_RND);
+        Collect myAfter = new Collect(6, Constants.PRIMORDIAL_RND);
+        Watermark myPointInTime = new Watermark(5, -1);
+
+        Assert.assertTrue(Protocol.isOldSeq.apply(myBefore, myPointInTime));
+        Assert.assertFalse(Protocol.isOldSeq.apply(myAfter, myPointInTime));
+    }
+
+    @Test
+    public void validateRndChecks() {
+        Collect myPast = new Collect(Constants.PRIMORDIAL_SEQ, 4);
+        Collect myElected = new Collect(Constants.PRIMORDIAL_SEQ, 6);
+
+        Assert.assertTrue(Protocol.isPastRnd.apply(myPast, myElected));
+        Assert.assertFalse(Protocol.isPastRnd.apply(myElected, myPast));
+
+        Collect myElectable = new Collect(Constants.PRIMORDIAL_SEQ, 8);
+
+        Assert.assertTrue(Protocol.isElectableRnd.apply(myElectable, myElected));
+        Assert.assertFalse(Protocol.isElectableRnd.apply(myPast, myElected));
+
+        Begin mySameRnd = new Begin(Constants.PRIMORDIAL_SEQ, myElected.getRndNumber(), null);
+        Assert.assertTrue(Protocol.isTheElectedRnd.apply(mySameRnd, myElected));
+
+        Assert.assertTrue(Protocol.shouldWriteCollect.apply(myElectable, myElected));
+        Assert.assertFalse(Protocol.shouldWriteCollect.apply(myPast, myElected));
+    }
+
+    @Test
+    public void validateCollectOld() {
+        Protocol.StateMachine myMachine = new Protocol.StateMachine();
+
+        // Old round
+        Assert.assertTrue(myMachine.old(new Collect(5, 10),
+                new Collect(5, 11),
+                new Watermark(5, -1)));
+
+        // Not electable
+        Assert.assertTrue(myMachine.old(new Collect(5, 11),
+                new Collect(5, 11),
+                new Watermark(5, -1)));
+
+        // Old sequence number
+        Assert.assertTrue(myMachine.old(new Collect(5, 10),
+                new Collect(5, 9),
+                new Watermark(6, -1)));
+    }
+
+    @Test
+    public void validateCollectActionable() {
+        Protocol.StateMachine myMachine = new Protocol.StateMachine();
+        InetSocketAddress myAddrOne = Utils.getTestAddress();
+        InetSocketAddress myAddrTwo = Utils.getTestAddress();
+
+        Assert.assertTrue("Same leader and greater round",
+                myMachine.actionable(new Collect(Constants.PRIMORDIAL_SEQ, 20),
+                myAddrOne, new Collect(Constants.PRIMORDIAL_SEQ, 19),
+                myAddrOne, 0));
+
+        Assert.assertTrue("Different leader but expired",
+                myMachine.actionable(new Collect(Constants.PRIMORDIAL_SEQ, 20),
+                myAddrOne, new Collect(Constants.PRIMORDIAL_SEQ, 19),
+                myAddrTwo, 0));
+
+        Assert.assertFalse("Different leader but not expired",
+                myMachine.actionable(new Collect(Constants.PRIMORDIAL_SEQ, 20),
+                myAddrOne, new Collect(Constants.PRIMORDIAL_SEQ, 19),
+                myAddrTwo, System.currentTimeMillis() + Leader.LeaseDuration.get() + 60000));
+
+        Assert.assertFalse("Different leader, expired but not electable",
+                myMachine.actionable(new Collect(Constants.PRIMORDIAL_SEQ, 19),
+                myAddrOne, new Collect(Constants.PRIMORDIAL_SEQ, 19),
+                myAddrTwo, 0));
+    }
+
+    @Test
+    public void validateBeginOld() {
+        Protocol.StateMachine myMachine = new Protocol.StateMachine();
+
+        Assert.assertTrue("Begin is for old round but newer sequence number",
+                myMachine.old(new Begin(2, 5, null),
+                new Collect(2, 6), new Watermark(1, -1)));
+
+        Assert.assertTrue("Begin is for current round but older sequence number",
+                myMachine.old(new Begin(1, 6, null),
+                        new Collect(2, 6), new Watermark(2, -1)));
+
+        Assert.assertFalse("Begin is for current round and newer sequence number",
+                myMachine.old(new Begin(3, 6, null),
+                        new Collect(2, 6), new Watermark(2, -1)));
+    }
+
+    @Test
+    public void validateBeginActionable() {
+        Protocol.StateMachine myMachine = new Protocol.StateMachine();
+
+        Assert.assertFalse("Not from elected, too low",
+                myMachine.actionable(new Begin(5, 4, null),
+                new Collect(5, 5)));
+
+        Assert.assertFalse("Not from elected, too high",
+                myMachine.actionable(new Begin(5, 10, null),
+                        new Collect(5, 5)));
+
+        Assert.assertTrue("From elected",
+                myMachine.actionable(new Begin(5, 10, null),
+                        new Collect(5, 10)));
+    }
+
+    static class ActCapture implements Protocol.Act {
+        boolean _acted = false;
+
+        private final Claim _successful;
+
+        ActCapture(Claim aSuccess) {
+            _successful = aSuccess;
+        }
+
+        @Override
+        public void accept(Claim aClaim, Boolean aWriteFlag) {
+            Assert.assertTrue(aWriteFlag);
+            Assert.assertEquals(_successful, aClaim);
+            _acted = true;
+        }
+    }
+
+    @Test
+    public void checkCollect() {
+        // StateMachine will be initialised with Collect.INITIAL, hence PRIMORDIAL rnd and sequence number
+        //
+        Protocol.StateMachine myMachine = new Protocol.StateMachine();
+        InetSocketAddress myProposer = Utils.getTestAddress();
+        long myCurrentExpiry = myMachine.getExpiry();
+
+        class OutdatedCapture implements Protocol.Outdated {
+            boolean _outdated = false;
+
+            @Override
+            public void accept(Long aLong, InetSocketAddress anAddress) {
+                Assert.assertTrue(Collect.INITIAL.getRndNumber() == aLong);
+                Assert.assertNotSame(myProposer, anAddress);
+                _outdated = true;
+            }
+        };
+
+        OutdatedCapture myOutdatedCapture = new OutdatedCapture();
+
+        myMachine.dispatch(new Collect(Constants.PRIMORDIAL_SEQ, 5),
+                myProposer, new Watermark(Constants.PRIMORDIAL_SEQ, -1),
+                myOutdatedCapture,
+                (c, w) -> {});
+
+        Assert.assertTrue("Primordial collect cannot superceed same", myOutdatedCapture._outdated);
+        Assert.assertEquals(myCurrentExpiry, myMachine.getExpiry());
+
+        Collect mySuccessful = new Collect(5, 5);
+
+        ActCapture myActCapture = new ActCapture(mySuccessful);
+        myOutdatedCapture._outdated = false;
+
+        myMachine.dispatch(mySuccessful, myProposer, new Watermark(4, -1),
+                myOutdatedCapture, myActCapture);
+
+        Assert.assertFalse("Newer collect should supercede primordial", myOutdatedCapture._outdated);
+        Assert.assertTrue(myActCapture._acted);
+        Assert.assertTrue(myCurrentExpiry < myMachine.getExpiry());
+    }
+
+    @Test
+    public void checkBegin() throws Exception {
+        // StateMachine will be initialised with Collect.INITIAL, hence PRIMORDIAL rnd and sequence number
+        //
+        Protocol.StateMachine myMachine = new Protocol.StateMachine();
+        InetSocketAddress myProposer = Utils.getTestAddress();
+        long myCurrentExpiry = myMachine.getExpiry();
+
+        class OutdatedCapture implements Protocol.Outdated {
+            boolean _outdated = false;
+
+            @Override
+            public void accept(Long aLong, InetSocketAddress anAddress) {
+                Assert.assertTrue(Collect.INITIAL.getRndNumber() == aLong);
+                Assert.assertNotSame(myProposer, anAddress);
+                _outdated = true;
+            }
+        };
+
+        OutdatedCapture myOutdatedCapture = new OutdatedCapture();
+
+        myMachine.dispatch(new Begin(Constants.PRIMORDIAL_SEQ - 1, Constants.PRIMORDIAL_RND, null),
+                new Watermark(Constants.PRIMORDIAL_SEQ, -1),
+                myOutdatedCapture,
+                (c, w) -> {});
+
+        Assert.assertTrue("Begin at primordial sequence - 1 should be declared outdated",
+                myOutdatedCapture._outdated);
+        Assert.assertEquals(myCurrentExpiry, myMachine.getExpiry());
+
+        Thread.sleep(200);
+
+        Begin mySuccessful = new Begin(5, Constants.PRIMORDIAL_RND, null);
+
+        ActCapture myActCapture = new ActCapture(mySuccessful);
+        myOutdatedCapture._outdated = false;
+
+        myMachine.dispatch(mySuccessful, new Watermark(4, -1),
+                myOutdatedCapture, myActCapture);
+
+        Assert.assertFalse(myOutdatedCapture._outdated);
+        Assert.assertTrue("Begin for later sequence and primordial rnd should progress", myActCapture._acted);
+        Assert.assertTrue(myCurrentExpiry < myMachine.getExpiry());
+    }
+}
